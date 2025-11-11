b37eef8ee931b7e59dc0be74091bdae1fce0a1ce
ReedDowns <radowns@mtu.edu>
2025-11-11 13:22:54 -0600

    Correct array initialization
    Correct initializing arrasy from arrays of pointers to arrays of floats.
    Correct randInit by adding array dimensional arguments to function call.
    Correct small typo in cudaMalloc.

--- fromScratchMNIST.cu
+++ fromScratchMNIST.cu
@@ -112,32 +112,32 @@ void main() {
 
     // Initialize arrays on host (before passing to device).
     /* Layer 0 - Input*/
-    float* iplHost[I*BS];          // Input layer, 784x1 
+    float iplHost[I*BS];          // Input layer, 784x1 
     /* Layer 1 - Hidden 1 */
-    float* weightsOneHost[H*I];     // Weights for layer one 128x784
-    float* biasOneHost[H*BS];          // Bias for layer 128x1
-    float* OPL_OneHost[H*BS];
+    float weightsOneHost[H*I];     // Weights for layer one 128x784
+    float biasOneHost[H*BS];          // Bias for layer 128x1
+    float OPL_OneHost[H*BS];
     // OPL1: 128xBS
     /* Layer 2 - Hidden 2 */
-    float* weightsTwoHost[H*H];     // Weights for layer two 128x128
-    float* biasTwoHost[H*BS];          // Bias for layer 2 128x1
-    float* OPL_TwoHost[H*BS];
+    float weightsTwoHost[H*H];     // Weights for layer two 128x128
+    float biasTwoHost[H*BS];          // Bias for layer 2 128x1
+    float OPL_TwoHost[H*BS];
     // OPL2: 128xBS
     /* Layer 3 - Reduction */
-    float* weightsThreeHost[O*H];
-    float* biasThreeHost[O*BS];
-    float* OPL_RedHost[O*BS];
+    float weightsThreeHost[O*H];
+    float biasThreeHost[O*BS];
+    float OPL_RedHost[O*BS];
     // OPL3: 10xBS 
     /* Layer 4 - Output */
-    float* oplFinalHost[O] = {0.f};  // Output layer; 10x1
+    float oplFinalHost[O] = {0.f};  // Output layer; 10x1
 
     // Initialize host values
-    randInit(weightsOneHost);
-    randInit(biasOneHost);
-    randInit(weightsTwoHost);
-    randInit(biasTwoHost);
-    randInit(weightsThreeHost);
-    randInit(biasTwoHost); 
+    randInit(weightsOneHost, H*I);
+    randInit(biasOneHost, H*BS);
+    randInit(weightsTwoHost, H*H);
+    randInit(biasTwoHost, H*BS);
+    randInit(weightsThreeHost, O*H);
+    randInit(biasThreeHost, O*BS); 
 
     // Set up device memory. cudaMalloc allocates device memory
     float *ipl, *weightsOne, *biasOne, *OPL_One;
@@ -147,7 +147,7 @@ void main() {
 
     CHECK_CUDA(cudaMalloc(&ipl, I * BS * sizeof(float)));
     CHECK_CUDA(cudaMalloc(&weightsOne, H * I * sizeof(float)));
-    CHECK_CUDA(cudaMallco(&biasOne, H * BS * sizeof(float)));
+    CHECK_CUDA(cudaMalloc(&biasOne, H * BS * sizeof(float)));
     CHECK_CUDA(cudaMalloc(&OPL_One, H * BS * sizeof(float)));
 
     CHECK_CUDA(cudaMalloc(&weightsTwo, H * H * sizeof(float)));
